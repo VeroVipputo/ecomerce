beba70a306ce287853989549a8e0edd7c01259cb
Diego Tettamanti <veronicavipputo@gmail.com>
2022-10-04 20:59:24 -0300

    commit40

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { formularioLogin, formularioRegistro, registrar, confirmar, principal, formularioOlvidePassword} from '../controllers/usuarioController.js';
+import { emailRegistro } from '../helpers/emails.js';
 
 
 const router = express.Router();
@@ -11,6 +12,7 @@ router.post('/main', principal);
 router.get('/registro', formularioRegistro);
 router.post('/registro', registrar);
 router.get('/confirmar/:token', confirmar);
+router.get('templates/mensaje', emailRegistro)
 router.get('/olvide-password', formularioOlvidePassword)
 
 
