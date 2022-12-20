fb7bbd0bbc5ab66f3d94cf2e0b117fb351cb4b0d
Diego Tettamanti <veronicavipputo@gmail.com>
2022-12-19 23:38:17 -0300

    commit84

--- seed/seeder.js
+++ seed/seeder.js
@@ -1,7 +1,9 @@
 import { exit } from 'node:process'
 import categorias from './categorias.js'
 import Categoria from '../models/Categoria.js'
+import Precio from '../models/Precio.js'
 import db from '../config/db.js'
+import precios from './precios.js'
 
 
 const importarDatos = async () => { 
@@ -13,7 +15,10 @@ const importarDatos = async () => {
         await db.sync()
 
         //Insertamos los datos
-        await Categoria.bulkCreate(categorias)
+        await Promise.all([
+        Categoria.bulkCreate(categorias),
+        Precio.bulkCreate(precios)
+        ])
         console.log('Datos importados Correctamente')
         exit()
 
@@ -22,9 +27,24 @@ const importarDatos = async () => {
         exit(1)
     }
 }
+const eliminarDatos = async () => {
+    try {
+        await db.sync({force:true})
+            console.log("Datos Eliminados correctamente");
+            exit()
+        
+    } catch (error) {
+        console.log(error)
+        exit(1)
+        
+    }
+}
 
 if(process.argv[2] === "-i") {
     importarDatos();
 }
 
+if(process.argv[2] === "-e") {
+    eliminarDatos();
+}
    
